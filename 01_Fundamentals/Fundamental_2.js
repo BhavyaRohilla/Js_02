60fcc7061e4db242d023b11f6ca06a87c9063594
Bhavya <officialbhavyarohilla@gmail.com>
2024-11-01 12:02:01 +0530

    D6

--- 01_Fundamentals/Fundamental_2.js
+++ 01_Fundamentals/Fundamental_2.js
@@ -91,7 +91,7 @@ function fruitProcessor(apples, oranges) {
 }
 
 console.log(fruitProcessor(2, 2));
-*/
+
 
 // Reviewing Function
 
@@ -112,3 +112,129 @@ const yearUntilRetirement = (birthYear, firstName) => {
 
 yearUntilRetirement(1991, "Ram");
 yearUntilRetirement(1955, "Mike");
+*/
+
+/*
+// Day - 6
+
+const friend1 = "Micheal";
+const friend2 = "Steven";
+const friend3 = "Peter";
+
+const friends = ["Micheal", "Steven", "Peter"];
+console.log(friends);
+
+const year = new Array(1991, 1992, 1993, 1994);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+friends[2] = "Jay";
+console.log(friends);
+
+// friends = [12, 45, 98]; Invalid
+const firstName = "Bhavya";
+const bhavya = [firstName, "Rohilla", 2024 - 2001, "Teacher", friends];
+console.log(bhavya[4][2]);
+
+// Exercise
+const calcAge = function (birthYear) {
+  return 2024 - birthYear;
+};
+const years = [1990, 1967, 2002, 2010, 2018];
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+console.log(age1, age2, age3);
+
+const ages = [
+  calcAge(years[0]),
+  calcAge(years[1]),
+  calcAge(years[years.length - 1]),
+];
+console.log(ages);
+
+/////////////////////////////////////////////////////////
+// Basic Array operations(Methods)
+const friends = ["Micheal", "Steven", "Peter"];
+
+// Add elements
+const newLength = friends.push("Jay");
+console.log(friends);
+console.log(newLength);
+
+friends.unshift("John");
+console.log(friends);
+
+// Remove elements
+const popped = friends.pop(); // Last
+console.log(popped);
+console.log(friends);
+
+friends.shift(); // First
+console.log(friends);
+
+console.log(friends.indexOf("Steven"));
+console.log(friends.indexOf("Bob"));
+
+friends.push(23);
+console.log(friends);
+console.log(friends.includes("Steven"));
+console.log(friends.includes("Bob"));
+console.log(friends.includes(23));
+
+if (friends.includes("Steven")) {
+  console.log("You have a friend called Steven");
+}
+
+*/
+//////////////////////////////////////
+// Objects
+
+const bhavyaArray = [
+  "Bhavya",
+  "Rohilla",
+  2024 - 2001,
+  "teacher",
+  ["Micheal", "Peter", "Steven"],
+];
+
+const bhavya = {
+  firstName: "Bhavya",
+  lastName: "Rohilla",
+  age: 2024 - 2001,
+  job: "teacher",
+  friends: ["Micheal", "Peter", "Steven"],
+};
+
+console.log(bhavya);
+console.log(bhavya.lastName);
+console.log(bhavya["lastName"]);
+
+const nameKey = "Name";
+console.log(bhavya["first" + nameKey]);
+console.log(bhavya["last" + nameKey]);
+
+// console.log(bhavya.'last' + nameKey)  // Invalid
+
+// const InterestedIn = prompt(
+//   "What do you want to know about Bhavya? choose between firstName, lastName, age, job, and friends"
+// );
+if (bhavya[InterestedIn]) {
+  console.log(bhavya[InterestedIn]);
+} else {
+  console.log(
+    "Wrong request! Choose Between firstName, lastName, age, job, and friends"
+  );
+}
+
+bhavya.location = "Haryana";
+bhavya["instagram"] = "Nope";
+console.log(bhavya);
+
+// Challenge
+// (Bhavya) has (3) friends, and his best friend is called (Micheal)
